685d4209f73b595fa0ef498a980757315f5a5819
Eyal.Kobrigo <eyal.kobrigo@clickdimensions.com>
2018-07-08 11:17:12 +0300

    continue to work on setpper motor added almost all the apis. startMove and stopMove are not finished

--- src/server/stepper-motor/stepper-motor-contoller.js
+++ src/server/stepper-motor/stepper-motor-contoller.js
@@ -8,32 +8,41 @@ if (config.developmentMode) {
 }
 
 function StepperMotorController(stepperPinsNumbers, homePin) {
-  this.homePin = homePin;
-  this.pins = [];
-
+  this.homePin = homePin; //the actual gpio pin
+  this.pins = []; //the actual gpio pin
   this.homePosition = 0;
-  this.currentPosition = 0;
-  this.homePositionReached = false;
+  this.currentPosition = undefined;
+  this.reachedHomePosition = false;
+  this.offsetFromHomeInSteps = 0;
+  this.numberOfStepsToMoveAwayFromHomeWhenReached = 3;
+  this.calibrating = false;
+  this.calibrated = false;
+  this.inMotion = false;
+  this.pendingStop = false;
+  this.maxPosition = 90;
 
   this.stepCounter = 0;
   this.stepTimeout = 0.01;
   this.stepCount = 8;
 
   this.Seq = [];
-  this.Seq[0] = [1,0,0,0];
-  this.Seq[1] = [1,1,0,0];
-  this.Seq[2] = [0,1,0,0];
-  this.Seq[3] = [0,1,1,0];
-  this.Seq[4] = [0,0,1,0];
-  this.Seq[5] = [0,0,1,1];
-  this.Seq[6] = [0,0,0,1];
-  this.Seq[7] = [1,0,0,1];
+  this.Seq[0] = [1, 0, 0, 0];
+  this.Seq[1] = [1, 1, 0, 0];
+  this.Seq[2] = [0, 1, 0, 0];
+  this.Seq[3] = [0, 1, 1, 0];
+  this.Seq[4] = [0, 0, 1, 0];
+  this.Seq[5] = [0, 0, 1, 1];
+  this.Seq[6] = [0, 0, 0, 1];
+  this.Seq[7] = [1, 0, 0, 1];
 
   if (!stepperPinsNumbers || stepperPinsNumbers.length != 4 || !homePin) {
     throw new Error('stepperPinsNumbers or homePin was not defined');
   }
 
-  this.setupPins(stepperPinsNumbers);
+  this.stepperPinsNumbers = stepperPinsNumbers;
+  this.homePin = homePin;
+
+  this.setupPins(this.stepperPinsNumbers);
 }
 
 StepperMotorController.prototype.setupPins = function (pinsNumbers) {
@@ -42,37 +51,151 @@ StepperMotorController.prototype.setupPins = function (pinsNumbers) {
   }.bind(this));
 };
 
-StepperMotorController.prototype.step = function () {
+StepperMotorController.prototype.init = function () {
+  this.sendSignalToPins();
+}
 
+StepperMotorController.prototype.sendSignalToPins = function () {
   for (let pin = 0; pin < 4; pin++) {
     if (this.Seq[this.stepCounter][pin] !== 0) {
+      //set the pin that needs to be set as 1 as 1;
       this.pins[pin].writeSync(1);
     } else {
+      //the rest of the pins should be set to 0
       this.pins[pin].writeSync(0);
     }
   }
-  this.stepCounter += 1
-  if (this.stepCounter === this.stepCount) {
-    this.stepCounter = 0;
+}
+
+StepperMotorController.prototype.step = function (clockWise) {
+  if (clockWise === undefined || clockWise === null) {
+    clockWise = true;
   }
-  if (this.stepCounter < 0) {
-    this.stepCounter = this.stepCount;
+
+  var incrementDirection = clockWise ? 1 : -1;
+  this.stepCounter += incrementDirection
+
+  //make it cyclic
+  if (clockWise) {
+    if (this.stepCounter === this.stepCount) {
+      this.stepCounter = 0;
+    }
+  } else {
+    if (this.stepCounter < 0) {
+      this.stepCounter = this.stepCount - 1;
+    }
+  }
+
+  this.sendSignalToPins();
+};
+
+StepperMotorController.prototype.calibrate = function (clockWise, callbackFunction) {
+  this.calibrating = true;
+  if (clockWise === undefined || clockWise === null) {
+    clockWise = true;
+  }
+
+  this._calibrateLoop(clockWise, callbackFunction);
+};
+
+StepperMotorController.prototype._moveAwayFromHomeLoop = function (clockWise, callbackFunction) {
+  var incrementDirection = clockWise ? 1 : -1;
+  if (this.offsetFromHomeInSteps !== this.numberOfStepsToMoveAwayFromHomeWhenReached) {
+    this.homePosition += incrementDirection;
+    this.offsetFromHomeInSteps++;
+    this.step(clockWise);
+
+    setTimeout(function () {
+      this._moveAwayFromHomeLoop(clockWise, callbackFunction);
+    }.bind(this), this.stepTimeout);
+  } else {
+    console.log('calibrated successfully'); //TODO add logging
+    this.calibrating = false;
+    this.calibrated = true;
+    this.currentPosition = 0;
+
+    if (typeof callbackFunction == 'function') {
+      callbackFunction();
+    }
   }
 };
 
-StepperMotorController.prototype.calibrate = function (clockWise) {
-  clockWise = clockWise || true;
-  this._calibrateLoop(clockWise);
+StepperMotorController.prototype._calibrateLoop = function (clockWise, callbackFunction) {
+  if (!this.reachedHomePosition) {
+    this.homePosition++;
+    this.step(clockWise);
+
+    //continue the loop
+    setTimeout(function () {
+      this._calibrateLoop(clockWise, callbackFunction);
+    }.bind(this), this.stepTimeout);
+
+  } else {
+    //stop the calibrate loop and start a sequence of moving away from the home position just a little bit.
+    this.offsetFromHomeInSteps = 0;
+    this._moveAwayFromHomeLoop(!clockWise, callbackFunction);
+  }
 };
 
-StepperMotorController.prototype._calibrateLoop = function (clockWise) {
-  if (!this.homePositionReached)  {
-    this.step();
+
+StepperMotorController.prototype._moveToPositionLoop = function (positionToMoveTo, finishedCallback) {
+  var directionToGoToIsClockWise = this.currentPosition < positionToMoveTo;
+  var positionIncement = directionToGoToIsClockWise ? 1 : -1;
+
+  if (this.currentPosition === positionToMoveTo) {
+    this.inMotion = false;
+    finishedCallback();
+  } else {
+    this.step(directionToGoToIsClockWise);
+    this.currentPosition += positionIncement;
+
+    if (this.currentPosition < 0 || this.currentPosition > this.maxPosition) {
+      throw new Error('position to move to is out of bounds. This should not have happened')
+    }
+
+    setTimeout(function () {
+      this._moveToPositionLoop(positionToMoveTo, finishedCallback);
+    }.bind(this), this.stepTimeout);
+  }
+};
+
+StepperMotorController.prototype.moveToPosition = function (positionToMoveTo, finishedCallback) {
+  if (!this.calibrated) {
+    throw new Error('Tried to move to a position before calibrating');
   }
 
-  setTimeout(function() {
-    this._calibrateLoop();
-  }.bind(this), this.stepTimeout);
+  this.inMotion = true;
+
+  this._moveToPositionLoop(positionToMoveTo, finishedCallback);
+};
+
+StepperMotorController.prototype.startMoving = function (clockWise) {
+  this.inMotion = true;
+};
+
+
+StepperMotorController.prototype._moveLoop = function (clockWise, stoppedCallback) {
+  var positionIncement = clockWise ? 1 : -1;
+
+  if (this.currentPosition < 0 || this.currentPosition > this.maxPosition || this.pendingStop) {
+    this.pendingStop = false;
+    this.inMotion = false;
+    if (typeof stoppedCallback == 'function') {
+      stoppedCallback();
+    }
+    return;
+  } else {
+    this.currentPosition += positionIncement;
+    this.step(clockWise);
+
+    setTimeout(function() {
+      this._moveLoop(clockWise);
+    })
+  }
+}
+
+StepperMotorController.prototype.stopMoving = function (clockWise) {
+  this.pendingStop = true;
 };
 
 module.exports = StepperMotorController;
--- src/server/stepper-motor/stepper-motor-controller.spec.js
+++ src/server/stepper-motor/stepper-motor-controller.spec.js
@@ -9,9 +9,11 @@ describe('StepperMotorController', function () {
   beforeEach(function () {
     clock = sinon.useFakeTimers();
     stepperMotorController = new StepperMotorController([1, 2, 3, 4], 5);
-    stepperMotorController.pins.forEach(function(pin) {
+    stepperMotorController.stepTimeout = 1; //this is done just for testing since the tick will not accept floats.
+    stepperMotorController.pins.forEach(function (pin) {
       sinon.spy(pin, 'writeSync')
     });
+    stepperMotorController.init();
 
     sinon.spy(stepperMotorController, 'step');
   });
@@ -34,16 +36,19 @@ describe('StepperMotorController', function () {
     expect(stepperMotorController.pins.length).to.equal(4);
   });
 
+  it('should call the initial signal to the pins when starting up', function () {
+    expect(stepperMotorController.pins[0].writeSync.calledOnce).to.equal(true);
+    expect(stepperMotorController.pins[1].writeSync.calledOnce).to.equal(true);
+    expect(stepperMotorController.pins[2].writeSync.calledOnce).to.equal(true);
+    expect(stepperMotorController.pins[3].writeSync.calledOnce).to.equal(true);
+    expect(stepperMotorController.pins[0].writeSync.withArgs(1).calledOnce).to.equal(true);
+    expect(stepperMotorController.pins[1].writeSync.withArgs(0).calledOnce).to.equal(true);
+    expect(stepperMotorController.pins[2].writeSync.withArgs(0).calledOnce).to.equal(true);
+    expect(stepperMotorController.pins[3].writeSync.withArgs(0).calledOnce).to.equal(true);
+  });
+
   describe('step', function () {
     it('should move the stepper motor one step at a time', function () {
-      stepperMotorController.step();
-      expect(stepperMotorController.pins[0].writeSync.calledOnce).to.equal(true);
-      expect(stepperMotorController.pins[1].writeSync.calledOnce).to.equal(true);
-      expect(stepperMotorController.pins[2].writeSync.calledOnce).to.equal(true);
-      expect(stepperMotorController.pins[3].writeSync.calledOnce).to.equal(true);
-      expect(stepperMotorController.pins[0].writeSync.withArgs(1).calledOnce).to.equal(true);
-      expect(stepperMotorController.pins[1].writeSync.withArgs(0).calledOnce).to.equal(true);
-
       stepperMotorController.step();
       expect(stepperMotorController.pins[0].writeSync.calledTwice).to.equal(true);
       expect(stepperMotorController.pins[1].writeSync.calledTwice).to.equal(true);
@@ -51,22 +56,162 @@ describe('StepperMotorController', function () {
       expect(stepperMotorController.pins[3].writeSync.calledTwice).to.equal(true);
       expect(stepperMotorController.pins[0].writeSync.withArgs(1).calledTwice).to.equal(true);
       expect(stepperMotorController.pins[1].writeSync.withArgs(1).calledOnce).to.equal(true);
-      expect(stepperMotorController.pins[1].writeSync.withArgs(0).calledOnce).to.equal(true);
+      expect(stepperMotorController.pins[2].writeSync.withArgs(0).calledTwice).to.equal(true);
+      expect(stepperMotorController.pins[3].writeSync.withArgs(0).calledTwice).to.equal(true);
+
+      stepperMotorController.step();
+      expect(stepperMotorController.pins[0].writeSync.lastCall.calledWith(0)).to.equal(true);
+      expect(stepperMotorController.pins[1].writeSync.lastCall.calledWith(1)).to.equal(true);
+      expect(stepperMotorController.pins[2].writeSync.lastCall.calledWith(0)).to.equal(true);
+      expect(stepperMotorController.pins[3].writeSync.lastCall.calledWith(0)).to.equal(true);
+
+      stepperMotorController.step();
+      expect(stepperMotorController.pins[0].writeSync.lastCall.calledWith(0)).to.equal(true);
+      expect(stepperMotorController.pins[1].writeSync.lastCall.calledWith(1)).to.equal(true);
+      expect(stepperMotorController.pins[2].writeSync.lastCall.calledWith(1)).to.equal(true);
+      expect(stepperMotorController.pins[3].writeSync.lastCall.calledWith(0)).to.equal(true);
+    });
+
+    it('should go back to the beginning of the sequence once it reached the end of it', function () {
+      for (var i = 0; i < stepperMotorController.stepCount - 1; i++) { //minus 1 since in the init we set the pins to the initial sequence
+        stepperMotorController.step();
+      }
+      //by now the last step on the sequence have been sent to the pins
+
+      stepperMotorController.step(); //this step should go back the the beginning of the sequence
+      expect(stepperMotorController.pins[0].writeSync.lastCall.calledWith(1)).to.equal(true);
+      expect(stepperMotorController.pins[1].writeSync.lastCall.calledWith(0)).to.equal(true);
+      expect(stepperMotorController.pins[2].writeSync.lastCall.calledWith(0)).to.equal(true);
+      expect(stepperMotorController.pins[3].writeSync.lastCall.calledWith(0)).to.equal(true);
+    });
+
+    it('should set counter clockwise if clockwise parameter is set to false', function () {
+      stepperMotorController.step(false);
+      expect(stepperMotorController.pins[0].writeSync.lastCall.calledWith(1)).to.equal(true);
+      expect(stepperMotorController.pins[1].writeSync.lastCall.calledWith(0)).to.equal(true);
+      expect(stepperMotorController.pins[2].writeSync.lastCall.calledWith(0)).to.equal(true);
+      expect(stepperMotorController.pins[3].writeSync.lastCall.calledWith(1)).to.equal(true);
+
+      stepperMotorController.step(false);
+      expect(stepperMotorController.pins[0].writeSync.lastCall.calledWith(0)).to.equal(true);
+      expect(stepperMotorController.pins[1].writeSync.lastCall.calledWith(0)).to.equal(true);
+      expect(stepperMotorController.pins[2].writeSync.lastCall.calledWith(0)).to.equal(true);
+      expect(stepperMotorController.pins[3].writeSync.lastCall.calledWith(1)).to.equal(true);
     });
 
   });
 
   describe('calibrate', function () {
     it('should start to move towards the home position and stop when the home position triggers', function () {
-      stepperMotorController.calibrate();
+      stepperMotorController.stepTimeout = 1; //this is done just for testing since the tick will not accept floats.
+      stepperMotorController.calibrate(false);
       expect(stepperMotorController.step.calledOnce).to.equal(true);
 
       clock.tick(stepperMotorController.stepTimeout);
       expect(stepperMotorController.step.callCount).to.equal(2);
 
-      clock.tick(stepperMotorController.stepTimeout * 6);
+      clock.tick(stepperMotorController.stepTimeout);
       expect(stepperMotorController.step.callCount).to.equal(3);
+
+      clock.tick(stepperMotorController.stepTimeout);
+      expect(stepperMotorController.step.callCount).to.equal(4);
+
+      //simulate that we have reached the home position
+      //TODO simulate this with the output from the pin not by changing the internal state
+      stepperMotorController.reachedHomePosition = true;
+      clock.tick(stepperMotorController.stepTimeout);
+      expect(stepperMotorController.step.callCount).to.equal(5);
+      expect(stepperMotorController.step.lastCall.calledWith(true)).to.equal(true);
+
+      //now it should start the small sequence of moving away from the home stopping pin 3 steps //TODO make 3 a parameter
+      clock.tick(stepperMotorController.stepTimeout);
+      expect(stepperMotorController.step.callCount).to.equal(6);
+      expect(stepperMotorController.step.lastCall.calledWith(true)).to.equal(true);
+      clock.tick(stepperMotorController.stepTimeout);
+      expect(stepperMotorController.step.callCount).to.equal(7);
+      expect(stepperMotorController.step.lastCall.calledWith(true)).to.equal(true);
+      clock.tick(stepperMotorController.stepTimeout);
+      expect(stepperMotorController.step.callCount).to.equal(7);
     });
+
+    it('should call the calibration callback function to signal it is done', function (done) {
+      stepperMotorController.calibrate(false, function () {
+        done();
+      });
+      clock.tick(stepperMotorController.stepTimeout);
+      stepperMotorController.reachedHomePosition = true;
+      clock.tick(stepperMotorController.stepTimeout);
+      clock.tick(stepperMotorController.stepTimeout);
+      clock.tick(stepperMotorController.stepTimeout);
+      clock.tick(stepperMotorController.stepTimeout);
+    });
+
+    it('should calibrate also when the home position is right from the start', function (done) {
+      stepperMotorController.reachedHomePosition = true;
+      stepperMotorController.calibrate(false, function () {
+        done();
+      });
+
+      clock.tick(stepperMotorController.stepTimeout);
+      clock.tick(stepperMotorController.stepTimeout);
+      clock.tick(stepperMotorController.stepTimeout);
+    });
+  });
+
+  describe('moveToPosition', function () {
+    beforeEach(function (done) {
+      stepperMotorController.calibrate(false, function () {
+        done();
+      });
+      clock.tick(stepperMotorController.stepTimeout);
+      stepperMotorController.reachedHomePosition = true;
+      clock.tick(stepperMotorController.stepTimeout);
+      clock.tick(stepperMotorController.stepTimeout);
+      clock.tick(stepperMotorController.stepTimeout);
+      clock.tick(stepperMotorController.stepTimeout);
+    });
+
+    it('should throw an exception if it is called before a calibration is done', function () {
+      stepperMotorController = new StepperMotorController([1, 2, 3, 4], 5);
+
+      expect(function () {
+        stepperMotorController.moveToPosition(45, function () {
+        });
+      }).to.throw();
+    });
+
+    it('should move to the requested position and call the given callback when done', function (done) {
+      expect(stepperMotorController.inMotion).to.equal(false);
+
+      stepperMotorController.moveToPosition(45, function () {
+        expect(stepperMotorController.currentPosition).to.equal(45);
+        expect(stepperMotorController.inMotion).to.equal(false);
+        done();
+      });
+
+      expect(stepperMotorController.inMotion).to.equal(true);
+
+      clock.tick(stepperMotorController.stepTimeout * 45);
+    });
+
+    it('should return error if the request to move to position is out of bounds in the first place', function () {
+      expect(true).to.equal(false);
+    });
+  });
+
+  describe('startMove and stopMove', function() {
+    it('should start the movement to the direction that is asked, continueing till stopMoved is called', function () {
+
+    });
+
+    it('should stop of it reaches the right limit', function () {
+
+    });
+
+    it('should stop if it reaches the left limit', function () {
+
+    });
+
   });
 
 });

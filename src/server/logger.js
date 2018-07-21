d1e823af970edf5edd10c221011c1d6078f17625
Eyal.Kobrigo <eyal.kobrigo@clickdimensions.com>
2018-07-21 12:57:37 +0300

    added a disable function to a prefixed logger

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,74 +1,88 @@
 var moment = require('moment');
 var _ = require('lodash');
-var util = require('util');
 var winston = require('winston');
 
 
 function logFormatterFunction(options) {
-    return options.timestamp().format('L HH:mm:ss:SSSS') + ' '+ options.level.toUpperCase() +' '+ (undefined !== options.message ? options.message : '') +
-        (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
+  return options.timestamp().format('L HH:mm:ss:SSSS') + ' ' + options.level.toUpperCase() + ' ' + (undefined !== options.message ? options.message : '') +
+    (options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '');
 }
 
 var consoleTransport, fileTransport;
 try {
-    consoleTransport = new (winston.transports.Console)({
-        level: 'debug',
-        timestamp: function() {
-            return moment();
-        },
-        formatter: logFormatterFunction
-    });
-    fileTransport = new (winston.transports.File)({
-        filename: './src/server/logs/server.log',
-        json: false,
-        level: 'debug',
-        maxFiles: 10,
-        maxsize: 2 * 1024 * 1024,
-        timestamp: function() {
-            return moment();
-        },
+  consoleTransport = new (winston.transports.Console)({
+    level: 'debug',
+    timestamp: function () {
+      return moment();
+    },
+    formatter: logFormatterFunction
+  });
+  fileTransport = new (winston.transports.File)({
+    filename: './src/server/logs/server.log',
+    json: false,
+    level: 'debug',
+    maxFiles: 10,
+    maxsize: 2 * 1024 * 1024,
+    timestamp: function () {
+      return moment();
+    },
 
-        formatter: logFormatterFunction
-    });
+    formatter: logFormatterFunction
+  });
 
 } catch (e) {
-   console.error(e);
+  console.error(e);
 }
 
 var winstonLogger = new (winston.Logger)({
-    transports: [
-        consoleTransport,
-        fileTransport
-    ]
+  transports: [
+    consoleTransport,
+    fileTransport
+  ]
 });
 
 var logger = {
-    createPrefixedLogger: function createPrefixedLogger(messagesPrefix) {
-        var returnedLogger = {};
-        _.forOwn(logger, function iterator(func, functionName) {
-           returnedLogger[functionName] = function(message){
-               var messageWithPerfix = messagesPrefix + ' ' + message;
-               func.call(this, messageWithPerfix);
-           };
-        });
+  _winstonLogger: winstonLogger,
+  createPrefixedLogger: function createPrefixedLogger(messagesPrefix) {
+    var returnedLogger = {
+      disabled: false,
+      disable: function (shouldDisable) {
+        this.disabled = shouldDisable;
+      },
+    };
+    _.forOwn(logger, function iterator(func, functionName) {
+      returnedLogger[functionName] = function (message) {
+        var messageWithPerfix = messagesPrefix + ' ' + message;
+        func.call(returnedLogger, messageWithPerfix);
+      };
+    });
 
-        return returnedLogger;
-    },
+    return returnedLogger;
+  },
 
-    log: function log(message) {
-        winstonLogger.info(message);
-    },
+  log: function log(message) {
+    if (this.disabled) return;
 
-    debug: function debug(message) {
-        winstonLogger.debug(message);
-    },
+    winstonLogger.info(message);
+  },
 
-    error: function error(message) {
-        winstonLogger.error(message);
-    },
-    warn: function warn(message) {
-        winstonLogger.warn(message);
-    }
+  debug: function debug(message) {
+    if (this.disabled) return;
+
+    winstonLogger.debug(message);
+  },
+
+  error: function error(message) {
+    if (this.disabled) return;
+
+    winstonLogger.error(message);
+  },
+
+  warn: function warn(message) {
+    if (this.disabled) return;
+
+    winstonLogger.warn(message);
+  }
 };
 
 module.exports = logger;

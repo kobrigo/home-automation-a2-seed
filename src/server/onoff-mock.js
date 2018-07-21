aacd776d900ff9948b267ee01837891a1a121c93
Eyal.Kobrigo <eyal.kobrigo@clickdimensions.com>
2018-07-21 12:59:38 +0300

    added a call to the logger on the onoff mock

--- src/server/onoff-mock.js
+++ src/server/onoff-mock.js
@@ -1,6 +1,11 @@
+var logger = require('./logger').createPrefixedLogger('OnOffMock:');
+logger.disable(true);
+
 module.exports = {
   Gpio: function (pin, config) {
+    var _pin = pin;
     this.writeSync = function (value) {
+      logger.log('writeSync pin:' + _pin  + ' value: ' + value);
     };
   }
-}
+};
